cfa82b000520e5fa3f02dd9ccfc2f5782da90000
VictorGSantosB <santosvgbento@gmail.com>
2024-05-09 00:12:15 -0300

    Ajustezinho

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -100,7 +100,7 @@
 
         function onToggleMenu(e) {
             e.name = e.name === 'menu' ? 'close' : 'menu'
-            navLinks.classList.toggle('top-[10%]')
+            navLinks.classList.toggle('top-[9%]')
         }
     </script>
 
